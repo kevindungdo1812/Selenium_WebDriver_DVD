8dd4169d5a74e33ba1d6b84ec34f8d45bdba454d
Dung <kevin.do1812@gmail.com>
2018-11-23 14:46:14 +0700

    test

---
+++ src/selenium_api/Topic_02_Xpath.java
@@ -0,0 +1,194 @@
+package selenium_api;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Xpath {
+	WebDriver driver;
+	
+ 
+  @BeforeClass
+  public void beforeClass() {
+
+				// Firefox
+				driver = new FirefoxDriver();
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+  }
+  
+  @Test
+  public void TC_01_VerifyUrlAndTitle() {
+	  
+	  //Step 01 - Truy cập vào trang: http://live.guru99.com/
+	  driver.get("http://live.guru99.com/");
+	 
+	  //Step 02 - Kiểm tra title của page là: "Home page"
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle, "Home page");
+	  Assert.assertTrue(homePageTitle.equals("Home page"));
+	  
+	  //Step 03 - Click vào link "My Account" để tới trang đăng nhập
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  
+	  //Step 04 - Click CREATE AN ACCOUNT button để tới trang đăng kí tài khoản
+	  driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+	  
+	  //Step 05 - Back lại trang đăng nhập
+	  driver.navigate().back();
+	  
+	  //Step 06 - Kiểm tra url của page đăng nhập là: http://live.guru99.com/index.php/customer/account/login/
+	  String loginUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(loginUrl, "http://live.guru99.com/index.php/customer/account/login/");
+	  
+	  //Step 07 - Forward tới trang tạo tài khoản
+	  driver.navigate().forward();
+	  
+	  //Step 08 - Kiểm tra url của page tạo tài khoản là: http://live.guru99.com/index.php/customer/account/create/
+	  String createAccountUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(createAccountUrl, "http://live.guru99.com/index.php/customer/account/create/");
+	}
+
+  @Test
+  public void TC_02_LoginEmpty() {
+	  
+	  //Step 01 - Truy cập vào trang: http://live.guru99.com/
+	  driver.get("http://live.guru99.com/");
+	  
+	  //Step 02 - Click vào link "My Account" để tới trang đăng nhập
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  
+	  //Step 03 - Để trống Username/ Password
+	  driver.findElement(By.name("login[username]")).sendKeys("");
+	  driver.findElement(By.name("login[password]")).sendKeys("");
+	  
+	 
+	  //Step 04 - Click Login button
+	  driver.findElement(By.id("send2")).click();
+	  
+	  //Step 05 - Verify error message xuất hiện tại 2 field:  This is a required field.
+	  String emailErrorMessage = driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).getText();
+	  Assert.assertEquals(emailErrorMessage, "This is a required field.");
+	  
+	  String passwordErrorMessage = driver.findElement(By.id("advice-required-entry-pass")).getText();
+	  Assert.assertEquals(passwordErrorMessage, "This is a required field.");
+	}
+  
+  @Test
+  public void TC_03_EmailIncorrectAndPasswordValid() {
+	  
+	  //Step 01 - Truy cập vào trang: http://live.guru99.com/
+	  driver.get("http://live.guru99.com/");
+	  
+	  //Step 02 - Click vào link "My Account" để tới trang đăng nhập
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  
+	  //Step 03 - Nhập email invalid: 123434234@12312.123123
+	  driver.findElement(By.name("login[username]")).sendKeys("123434234@12312.123123");
+	  driver.findElement(By.name("login[password]")).sendKeys("");
+	  
+	  //Step 04 - Click Login button
+	  driver.findElement(By.id("send2")).click();
+	  
+	  //Step 05 - Verify error message xuất hiện:  Please enter a valid email address. For example johndoe@domain.com.
+	  String emailInvalidMessage = driver.findElement(By.xpath("//div[@id='advice-validate-email-email']")).getText();
+	  Assert.assertEquals(emailInvalidMessage, "Please enter a valid email address. For example johndoe@domain.com.");
+	  
+	}
+
+
+  @Test
+  public void TC_04_EmailCorrectAndPasswordLessThan6Chars() {
+	 
+	  //Step 01 - Truy cập vào trang: http://live.guru99.com/
+	  driver.get("http://live.guru99.com/");
+	  
+	  //Step 02 - Click vào link "My Account" để tới trang đăng nhập
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  
+	  //Step 03 - Nhập email correct and password incorrect: automation@gmail.com/ 123
+	  driver.findElement(By.name("login[username]")).sendKeys("automation@gmail.com");
+	  driver.findElement(By.name("login[password]")).sendKeys("123");
+	  
+	  //Step 04 - Click Login button
+	  driver.findElement(By.id("send2")).click();
+	  
+	  //Step 05 - Verify error message xuất hiện: Please enter 6 or more characters without leading or trailing spaces.
+	  String passwordInvalidMessage = driver.findElement(By.xpath("//div[@id='advice-validate-password-pass']")).getText();
+	  Assert.assertEquals(passwordInvalidMessage, "Please enter 6 or more characters without leading or trailing spaces.");
+	  
+	}
+  
+  @Test
+  public void TC_05_EmailCorrectAndPasswordIncorrect() {
+	 
+	  //Step 01 - Truy cập vào trang: http://live.guru99.com/
+	  driver.get("http://live.guru99.com/");
+	  
+	  //Step 02 - Click vào link "My Account" để tới trang đăng nhập
+	  driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	  
+	  //Step 03 - Nhập email correct and password incorrect: automation@gmail.com/ 123123123
+	  driver.findElement(By.name("login[username]")).sendKeys("automation@gmail.com");
+	  driver.findElement(By.name("login[password]")).sendKeys("123123123");
+	  
+	  //Step 04 - Click Login button
+	  driver.findElement(By.id("send2")).click();
+	  
+	  //Step 05 - Verify error message xuất hiện: Invalid login or password.
+	  Assert.assertTrue(driver.findElement(By.xpath("//span[text()='Invalid login or password.']")).isDisplayed());
+	  
+	}
+  
+  @Test
+  public void TC_06_RegisterToSystem() {
+	  String firstname ="Selenium", lastname = "online 07", email = "seleniumonline" + Commons.randomEmail() + "@gmail.com", password = "123123";
+	  System.out.println("Email random =" + email);
+	  //Step 01 - Truy cập vào trang: http://live.guru99.com/
+	 driver.get("http://live.guru99.com/");
+	 
+	 //Step 02 - Click vào link "My Account" để tới trang đăng nhập
+	 driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+	 
+	 //Step 03 - Click CREATE AN ACCOUNT button để tới trang đăng kí tài khoản
+	 driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+	 
+	 //Step 04 - Nhập thông tin hợp lệ vào tất cả các field: First Name/ Last Name/ Email Address/ Password/ Confirm Password
+	 driver.findElement(By.id("firstname")).sendKeys(firstname);
+	 driver.findElement(By.name("lastname")).sendKeys(lastname);
+	 driver.findElement(By.id("email_address")).sendKeys(email);
+	 driver.findElement(By.className("validate-password")).sendKeys(password);
+	 driver.findElement(By.className("validate-cpassword")).sendKeys(password);
+	 
+	 //Step 05 - Click REGISTER button
+	 driver.findElement(By.xpath("//button[@title= 'Register']")).click();
+	 
+	 //Step 06 - Verify message xuất hiện khi đăng kí thành công: Thank you for registering with Main Website Store.
+	 Assert.assertTrue(driver.findElement(By.xpath("//span[text()='Thank you for registering with Main Website Store.']")).isDisplayed());
+	  
+	 //Step 07 - Logout khỏi hệ thống
+	 driver.findElement(By.xpath("//div[@class =\"page-header-container\"]//span[text()='Account']")).click();
+	 driver.findElement(By.xpath("//a[text() = 'Log Out']")).click();
+	 
+	 //Step 08 - Kiểm tra hệ thống navigate về Home page sau khi logout thành công
+	 Assert.assertTrue(driver.findElement(By.xpath("//h2[contains(text(),'This is demo site for')]")).isDisplayed());
+	}
+  
+  	//Update commit
+
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+  
+ 
+}
---
+++ src/selenium_api/Topic_04_Textbox.java
@@ -0,0 +1,151 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_04_Textbox {
+	WebDriver driver;
+	private String newName, newDob, newAddress, newCity, newState, newPin, newMobile, newEmail, password;
+	private String editAddress, editCity, editState, editPin, editMobile, editEmail, customerID;
+	
+	
+	By nameByTextbox = By.xpath("//input[@name='name']");
+	By dobByTextbox = By.xpath("//input[@name='dob']");
+	By addrByTextarea = By.xpath("//textarea[@name='addr']");
+	By cityByTextbox = By.xpath("//input[@name='city']");
+	By stateByTextbox = By.xpath("//input[@name='state']");
+	By pinByTextbox = By.xpath("//input[@name='pinno']");
+	By mobileByTextbox = By.xpath("//input[@name='telephoneno']");
+	By emailByTextbox = By.xpath("//input[@name='emailid']");
+	By passwordByTextbox = By.xpath("//input[@name='password']");
+	By submitByButton = By.xpath("//input[@name='sub']");
+	
+ 
+  @BeforeClass
+  public void beforeClass() {
+
+		driver = new FirefoxDriver();
+		driver.get("http://demo.guru99.com/v4/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+				
+		newName ="Selenium Online";
+		newDob ="2000-10-01";
+		newAddress ="123 Address";
+		newCity ="Ho Chi Minh";
+		newState ="Thu Duc";
+		newPin ="123456";
+		newMobile ="0123456789";
+		newEmail ="autotest" + Commons.randomEmail() +"@gmail.com";
+		password ="123456";
+			
+		editAddress ="789 Edit Address";
+		editCity ="Paris";
+		editState ="Premier";
+		editPin ="987654";
+		editMobile ="0987456123";
+		editEmail ="edittest" + Commons.randomEmail() +"@gmail.com";
+  }
+  
+  @Test
+  public void TC_01_NewCustomer() {
+	  driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr161493");
+	  driver.findElement(By.xpath("//input[@name='password']")).sendKeys("harErAh");
+	  driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+	  Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
+	  
+	  driver.findElement(By.xpath("//a[text()='New Customer']")).click();
+	  
+	  driver.findElement(nameByTextbox).sendKeys(newName);
+	  driver.findElement(dobByTextbox).sendKeys(newDob);
+	  driver.findElement(addrByTextarea).sendKeys(newAddress);
+	  driver.findElement(cityByTextbox).sendKeys(newCity);
+	  driver.findElement(stateByTextbox).sendKeys(newState);
+	  driver.findElement(pinByTextbox).sendKeys(newPin);
+	  driver.findElement(mobileByTextbox).sendKeys(newMobile);
+	  driver.findElement(emailByTextbox).sendKeys(newEmail);
+	  driver.findElement(passwordByTextbox).sendKeys(password);
+	  driver.findElement(submitByButton).click();
+	  
+	  /*Get customerID*/
+	  customerID = driver.findElement(By.xpath("//td[text()='Customer ID']/following-sibling::td")).getText();
+	  System.out.println("Customer ID =" + customerID);
+	  
+	  /*Verify Input Data mathching vs Output Data*/
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText(), newName);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Birthdate']/following-sibling::td")).getText(), newDob);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(), newAddress);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), newCity);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(), newState);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), newPin);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(), newMobile);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(), newEmail);
+	  
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Birthdate']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText());
+	  
+	}
+
+  @Test
+  public void TC_02_EditCustomer() {
+	  
+	  driver.findElement(By.xpath("//a[text()='Edit Customer']")).click();
+	  driver.findElement(By.xpath("//input[@name='cusid']")).sendKeys(customerID);
+	  driver.findElement(By.xpath("//input[@name='AccSubmit']")).click();
+	  
+	  /*Verify Customer name and Address*/
+	  Assert.assertEquals(driver.findElement(nameByTextbox).getAttribute("value"), newName);
+	  Assert.assertEquals(driver.findElement(addrByTextarea).getText(), newAddress);
+	  
+	  /*Edit Customer - Input Data*/
+	  driver.findElement(addrByTextarea).clear();
+	  driver.findElement(cityByTextbox).clear();
+	  driver.findElement(stateByTextbox).clear();
+	  driver.findElement(pinByTextbox).clear();
+	  driver.findElement(mobileByTextbox).clear();
+	  driver.findElement(emailByTextbox).clear();
+	  driver.findElement(addrByTextarea).sendKeys(editAddress);
+	  driver.findElement(cityByTextbox).sendKeys(editCity);
+	  driver.findElement(stateByTextbox).sendKeys(editState);
+	  driver.findElement(pinByTextbox).sendKeys(editPin);
+	  driver.findElement(mobileByTextbox).sendKeys(editMobile);
+	  driver.findElement(emailByTextbox).sendKeys(editEmail);
+	  driver.findElement(submitByButton).click();
+	  
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(), editAddress);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), editCity);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(), editState);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), editPin);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(), editMobile);
+	  Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(), editEmail);
+	  
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText());
+	  System.out.println(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText());
+	  
+	}
+
+  //Update
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
